964114c190fbc63ffd2205c3b1f64124a4da1043
rbadheka1 <64173495+rbadheka1@users.noreply.github.com>
2025-07-04 19:55:47 +0100

    Create LoginSteps.java

---
+++ src/test/java/com/elarenbee/mobile/automation/steps/LoginSteps.java
@@ -0,0 +1,23 @@
+package com.elarenbee.mobile.automation.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+    @Given("the user is on the login screen")
+    public void theUserIsOnTheLoginScreen() {
+        // Code to ensure the user is on the login screen
+    }
+
+    @When("the user enters valid credentials")
+    public void theUserEntersValidCredentials() {
+        // Code to simulate entering valid credentials
+    }
+
+    @Then("the user should be logged in successfully")
+    public void theUserShouldBeLoggedInSuccessfully() {
+        // Code to verify successful login
+    }
+}
