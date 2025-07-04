3977683522eb140f78805787cb39b700291d13e8
rbadheka1 <64173495+rbadheka1@users.noreply.github.com>
2025-07-04 20:22:32 +0100

    Create ConfigReader.java

---
+++ src/test/java/com/elarenbee/mobile/automation/utils/ConfigReader.java
@@ -0,0 +1,27 @@
+package com.elarenbee.mobile.automation.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            System.out.println("Could not load config.properties, relying on environment variables.");
+        }
+    }
+
+    public static String getProperty(String key) {
+        // Prefer environment variable over properties file
+        String envValue = System.getenv(key);
+        if (envValue != null) {
+            return envValue;
+        }
+        return properties.getProperty(key);
+    }
+}
