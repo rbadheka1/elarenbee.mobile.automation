b36fc57d6b846ed1933a8adff6a33e262a793b07
rbadheka1 <64173495+rbadheka1@users.noreply.github.com>
2025-07-04 19:53:23 +0100

    Create SampleSteps.java

---
+++ src/test/java/com/elarenbee/mobile/automation/steps/SampleSteps.java
@@ -0,0 +1,23 @@
+package com.elarenbee.mobile.automation.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SampleSteps {
+
+    @Given("the app is launched")
+    public void theAppIsLaunched() {
+        // Code to verify the app is launched
+    }
+
+    @When("the user performs a sample action")
+    public void theUserPerformsSampleAction() {
+        // Code to perform sample action
+    }
+
+    @Then("the expected result occurs")
+    public void theExpectedResultOccurs() {
+        // Code to verify expected result
+    }
+}
