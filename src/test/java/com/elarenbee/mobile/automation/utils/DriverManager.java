67d0a010262ac7ccd4d3de7ff89d5ec90e442ca7
rbadheka1 <64173495+rbadheka1@users.noreply.github.com>
2025-07-05 17:43:29 +0100

    Update DriverManager.java
    Add DriverManager utility for thread-safe AppiumDriver management
    
    - Use ThreadLocal to manage AppiumDriver instances per thread
    - Provide methods to set, get, and quit the AppiumDriver instance

--- src/test/java/com/elarenbee/mobile/automation/utils/DriverManager.java
+++ src/test/java/com/elarenbee/mobile/automation/utils/DriverManager.java
@@ -2,7 +2,6 @@ package com.elarenbee.mobile.automation.utils;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class DriverManager {
     private static final ThreadLocal<AppiumDriver<MobileElement>> driver = new ThreadLocal<>();
